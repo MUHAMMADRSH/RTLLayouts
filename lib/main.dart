462a37f3bd350b4ff73fc5858aa486a3e15a250c
mohammad <daneshjoo1396isfahan@gmail.com>
2019-07-02 09:41:54 +0430

    way 2: force local on all device

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale("fa", "IR"), // OR Locale('ar', 'AE') OR Other RTL locales
+      ],
+      locale: Locale("fa", "IR"),
+      home: Scaffold(
+        appBar: AppBar(
+
+          title: Text("لایه راست به چپ",textDirection: TextDirection.rtl,),
+        ),
+        body:Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text("سلام")
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
 }
